cac6c2af52137ac515988bbf30225f98bec74ecb
Sabah Syed <sabisyed@gmail.com>
2020-02-26 20:46:05 -0800

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,14 @@
+function grow(){
+    document.getElementById("box").style.height = "500px";
+}
+
+function blue(){
+    document.getElementById("box").style.backgroundColor = "blue";
+}
+function fade(){
+    document.getElementById("box").style.opacity = 0.5;
+}
+function reset(){
+  document.getElementById("box").style =
+    "height:150px; width:150px; background-color:orange; margin:25px";
+} 
